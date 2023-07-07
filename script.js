c9920e994b39d821da422bcb356c7a88fb3651de
minamky <minaky@stanford.edu>
2023-07-07 00:28:57 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+//selecting html elements
+let nameElement = document.querySelector('#name');
+let emailElement = document.querySelector('#email');
+let messageElement = document.querySelector("#message");
+
+let submitButton = document.querySelector('#submit-button');
+submitButton.addEventListener('click', function(e) {
+	e.preventDefault();
+
+	let nameValue = nameElement.value;
+	let emailValue = emailElement.value;
+	let messageValue = messageElement.value;
+
+	if(emailValue.includes('@')){
+		console.log('Name: ', nameValue);
+		console.log('Email: ', emailValue);
+		console.log('Message: ', messageValue)
+		alert('thank you for your message!');
+		document.forms['contact-form'].reset()
+	} else {
+		alert(emailValue + " is an invalid email address, please try again!")
+	}
+});
